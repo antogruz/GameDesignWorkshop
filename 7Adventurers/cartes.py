b374bbf07479efa6c29a2bfd9da873be3af637eb
Antoine Gruzelle <antoine.gruzelle@allegrodvt.com>
2016-08-16 16:59:16 +0200

    Tests sur la valeur des cartes en moyenne

---
+++ 7Adventurers/cartes.py
@@ -0,0 +1,52 @@
+def contactValue(degats, defense, ennemi):
+    if degats > ennemi.d:
+        return ennemi.value() - (ennemi.a - defense)/float(2)
+    else:
+        return (degats / float(ennemi.d)) * ennemi.value() - (ennemi.a - defense)/float(2)
+
+def distanceValue(degats, ennemi):
+    if degats > ennemi.d:
+        return ennemi.value()
+    else:
+        return (degats / float(ennemi.d)) * ennemi.value()
+
+
+class Hammer:
+    @staticmethod
+    def value(hero, enn):
+        return contactValue(4 + hero.force, hero.defense, enn)
+
+class Dagues:
+    @staticmethod
+    def value(hero, enn):
+        return 2 * contactValue(2 + hero.force, hero.defense, enn)
+
+class Shurikens:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    def value(self, hero, enn):
+        return self.nombre * distanceValue(1, enn)
+
+class ArcElfique:
+    @staticmethod
+    def value(hero, enn):
+        return distanceValue(2, enn) + 0.5
+
+class Javelot:
+    @staticmethod
+    def value(hero, enn):
+        return distanceValue(2 + hero.force, enn)
+
+class Enchevetrement:
+    def __init__(self, nombre, mana):
+        self.nombre = nombre
+        self.mana = mana
+    def value(self, hero, enn):
+        return (self.nombre - 1) * distanceValue(hero.terre, enn) + distanceValue(3, enn) - 0.5*self.mana
+
+class IceSword:
+    def __init__(self, mana):
+        self.mana = mana
+    def value(self, hero, enn):
+        return contactValue(4 + hero.force + hero.eau, hero.defense + hero.eau, enn) - 0.5 * self.mana
+
--- 7Adventurers/value.py
+++ 7Adventurers/value.py
@@ -1,3 +1,5 @@
+import cartes
+
 def nbhits(d):
     return d / float(3)
 
@@ -9,13 +11,32 @@ class Enn:
     def value(self):
         return self.d + self.a * nbhits(self.d)/float(4)
 
-def hammerValue(force, ennemi):
-    if force > ennemi.d:
-        return ennemi.value() - ennemi.a/float(2)
-    else:
-        return (force / float(ennemi.d)) * ennemi.value() - ennemi.a/float(2)
+class Hero:
+    def __init__(self, name, force=0, defense=0, terre=0, eau=0, feu=0):
+        self.name = name
+        self.force = force
+        self.defense = defense
+        self.terre = terre
+        self.eau = eau
+        self.feu = feu
 
-ennemis = [Enn(a, d) for a in range(0, 7) for d in range(2, 7) if (a + d) < 10 ]
+def moyenne(list):
+    somme = 0
+    for l in list:
+        somme += l
+    return somme / len(list)
+ennemis = [Enn(a, d) for d in range(2, 7) for a in range(0, 2*d + 1) if (a + d) < 10 ]
 
-for enn in ennemis:
-    print str(enn.a) + "-" + str(enn.d) + " " + str(hammerValue(4, enn))
+def evaluate(hero, card):
+    values = [card.value(hero, enn) for enn in ennemis]
+    return round(moyenne(values), 2)
+
+def main():
+    heroes = [Hero(name="fighter",force=1), Hero(name="tank",defense=1), Hero(name="druide", terre=1), Hero(name="aquaman", eau=1), Hero(name="pyro", feu=1)]
+    cards = [cartes.Hammer(), cartes.Dagues(), cartes.Shurikens(3), cartes.ArcElfique(), cartes.Javelot(), cartes.Enchevetrement(nombre=3, mana=3), cartes.IceSword(mana=1)]
+    for carte in cards:
+        print "--- {} ---".format(carte.__class__.__name__)
+        for hero in heroes:
+            print "{} : {}".format(hero.name, evaluate(hero, carte))
+
+main()
